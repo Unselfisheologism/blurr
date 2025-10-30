e3b620a608bfa353cd92f7b29183fb292d2edc7c
Unselfisheologism <jeffrinjames99@gmail.com>
2025-10-30 14:49:26 +0530

    changes manual

--- app/src/main/java/com/blurr/voice/LoginActivity.kt
+++ app/src/main/java/com/blurr/voice/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.webkit.WebView
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
